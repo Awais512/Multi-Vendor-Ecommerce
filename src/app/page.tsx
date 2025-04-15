c8ff36fcbabde3fd810613d1397406b2933d09c3
awais512 <raza.awais25@gmail.com>
2025-04-15 23:23:40 +0100

    Style Customization

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,20 @@
+import { Button } from "@/components/ui/button";
+import { Checkbox } from "@/components/ui/checkbox";
+import { Input } from "@/components/ui/input";
+import { Progress } from "@/components/ui/progress";
+import { Textarea } from "@/components/ui/textarea";
+
 export default function Home() {
   return (
-    <div>
+    <div className="flex flex-col gap-y-4 p-4">
       <p className="text-rose-500">Hello World</p>
+      <div className="flex w-full gap-2">
+        <Button variant="elevated">I am Button</Button>
+        <Input placeholder="Input" className="w-64" />
+        <Progress value={50} className="w-64" />
+        <Textarea value="Hello" className="w-64" />
+        <Checkbox value="One" />
+      </div>
     </div>
   );
 }
