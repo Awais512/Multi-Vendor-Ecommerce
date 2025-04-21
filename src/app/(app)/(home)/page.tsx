914813c3580d7c0e2b5b1ffa3c1ee203dbcc1127
awais512 <raza.awais25@gmail.com>
2025-04-21 13:32:32 +0100

    TRPC Setup Done

--- src/app/(app)/(home)/page.tsx
+++ src/app/(app)/(home)/page.tsx
@@ -1,3 +1,15 @@
+"use client";
+
+import { useTRPC } from "@/trpc/client";
+import { useQuery } from "@tanstack/react-query";
+
 export default function Home() {
-  return <div className="flex flex-col gap-y-4 p-4">Homepage</div>;
+  const trpc = useTRPC();
+  const categories = useQuery(trpc.categories.getMany.queryOptions());
+
+  if (categories.isLoading) {
+    return <p>Loading...</p>;
+  }
+
+  return <div>{JSON.stringify(categories.data)}</div>;
 }
