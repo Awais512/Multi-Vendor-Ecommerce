83b2217c13628f1b76684f6e4ef7f04912b0285d
awais512 <raza.awais25@gmail.com>
2025-05-11 11:44:04 +0100

    filtering products UI

--- src/app/(app)/layout.tsx
+++ src/app/(app)/layout.tsx
@@ -3,6 +3,7 @@ import { DM_Sans } from "next/font/google";
 import "./globals.css";
 import { TRPCReactProvider } from "@/trpc/client";
 import { Toaster } from "@/components/ui/sonner";
+import { NuqsAdapter } from "nuqs/adapters/next/app";
 
 const font = DM_Sans({
   subsets: ["latin"],
@@ -21,10 +22,12 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={`${font.className} antialiased`}>
-        <TRPCReactProvider>
-          {children}
-          <Toaster />
-        </TRPCReactProvider>
+        <NuqsAdapter>
+          <TRPCReactProvider>
+            {children}
+            <Toaster />
+          </TRPCReactProvider>
+        </NuqsAdapter>
       </body>
     </html>
   );
--- src/modules/products/server/procedure.ts
+++ src/modules/products/server/procedure.ts
@@ -8,10 +8,24 @@ export const productsRouter = createTRPCRouter({
     .input(
       z.object({
         category: z.string().nullable().optional(),
+        minPrice: z.string().nullable().optional(),
+        maxPrice: z.string().nullable().optional(),
       })
     )
     .query(async ({ ctx, input }) => {
       const where: Where = {};
+
+      if (input.minPrice) {
+        where.price = {
+          greater_than_equal: input.minPrice,
+        };
+      }
+
+      if (input.maxPrice) {
+        where.price = {
+          less_than_equal: input.maxPrice,
+        };
+      }
 
       if (input.category) {
         const categoryData = await ctx.db.find({
@@ -44,7 +58,7 @@ export const productsRouter = createTRPCRouter({
           );
         }
         where["category.slug"] = {
-          in: [parentCategory.slug, ...subcategoriesSlug],
+          in: [parentCategory?.slug, ...subcategoriesSlug],
         };
       }
 
