4f122410c1ede6ad0e3a0c26e60631663c2b18e8
awais512 <raza.awais25@gmail.com>
2025-05-13 00:14:59 +0100

    Tags Router

--- src/trpc/routers/_app.ts
+++ src/trpc/routers/_app.ts
@@ -2,10 +2,12 @@ import { authRouter } from "@/modules/auth/server/procedures";
 import { createTRPCRouter } from "../init";
 import { categoriesRouter } from "@/modules/categories/server/procedure";
 import { productsRouter } from "@/modules/products/server/procedure";
+import { tagsRouter } from "@/modules/tags/server/procedure";
 export const appRouter = createTRPCRouter({
   auth: authRouter,
   products: productsRouter,
   categories: categoriesRouter,
+  tags: tagsRouter,
 });
 // export type definition of API
 export type AppRouter = typeof appRouter;
